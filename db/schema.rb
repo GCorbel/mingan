8ff50a47d4a18e36a7fa04e1620603a9b103dbc0
GCorbel <guirec.corbel@gmail.com>
2014-06-08 08:26:05 -0400

    use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140605204545) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sightings", force: true do |t|
     t.date     "date"
     t.string   "region"
